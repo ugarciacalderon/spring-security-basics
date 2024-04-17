20cae391baf4583998203fdc8f804ab868497ed3
Ulises Garcia <ugarciacalderon@gmail.com>
2024-04-16 18:12:57 -0600

    added project spring

---
+++ src/test/java/com/ugarciac/springsecuritybasics/SpringsecuritybasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugarciac.springsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
